279400112db000ed226e1af2ef4b7039867be29b
Sinmbf <sinmbf123@gmail.com>
2023-10-23 15:51:16 +0545

    Started backend for form

--- iBlog/backend/index.js
+++ iBlog/backend/index.js
@@ -2,6 +2,10 @@ import express from "express";
 const app = express();
 const port = 5000;
 
+app.get("/", (req, res) => {
+  res.send("Hello Sinmbf");
+});
+
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
 });
