e73c89ced09c81886154296c94bfec150219eb1e
Sinmbf <sinmbf123@gmail.com>
2023-11-11 19:17:40 +0545

    Removed id from chat

--- MERN-AI-ChatBot-starting-project/backend/src/models/User.ts
+++ MERN-AI-ChatBot-starting-project/backend/src/models/User.ts
@@ -1,10 +1,6 @@
 import { Schema, model } from "mongoose";
 
 const ChatSchema = new Schema({
-  id: {
-    type: String,
-    default: crypto.randomUUID(),
-  },
   role: {
     type: String,
     required: true,
