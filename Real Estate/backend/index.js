199dbf2f2f94a2a54bab1bdf57e4778741bb2ef6
Sinmbf <sinmbf123@gmail.com>
2024-07-28 00:09:02 +0545

    Connect To MongoDB

--- Real Estate/backend/index.js
+++ Real Estate/backend/index.js
@@ -1,8 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+// Configure environment variable
+dotenv.config();
 
+// Create express app
 const app = express();
 const PORT = 3000;
 
+// Connect To MongoDb Atlas
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("Connected To MongoDB Atlas successfully");
+  })
+  .catch((err) => console.log(err));
+
+// Run server on port
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
